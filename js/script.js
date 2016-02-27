30da97d4ad575d017fc53888c200e40278a657ea
Hubert Theodore <hubert@yup.com>
2016-02-27 12:28:24 -0600

    js initialized

---
+++ js/script.js
@@ -0,0 +1,9 @@
+'use strict';
+const $ = require('jQuery');
+const superagent = require('superagent');
+
+(function(){
+    console.log($);
+    console.log(superagent);
+    console.log("jquery and superagent is loaded!");
+})();
