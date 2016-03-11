85a09135a70c8b4b2ec705139e17971d005cc86c
Russell Ang <russ@wirelessprvnat-172-17-127-92.near.illinois.edu>
2016-03-10 20:15:24 -0600

    navbar with search state working

--- assets/compiled/js/script.js
+++ assets/compiled/js/script.js
@@ -2,8 +2,45 @@ global.jQuery = require('jQuery');
 const $ = require('jQuery');
 const bootstrap=require('bootstrap');
 $(document).ready(function(){
-    $('[data-toggle="popover"]').popover();   
+    $('[data-toggle="popover"]').popover({
+        delay:{hide:"100"},
+            content: "<p>Dummy Text</p>",
+            html:true,
+    });   
 });
+function changeBreadcrumbContent(parentCategory,parentName,childCategory,childName){
+    Document.getElementById('Kingdom-category').innerHTML=parentCategory;
+    Document.getElementById('Kingdom-name').innerHTML=parentName;
+    Document.getElementById('Phylum-category').innerHTML=childCategory;
+    Document.getElementById('Phylum-name').innerHTML=childName;
+}
+var searchState = "all";
+function changeSearchState(){
+    if(searchState=="parent"){
+        document.getElementById("search-state").innerHTML='&#9680';
+    }
+    else if(searchState=="child"){
+        document.getElementById("search-state").innerHTML='&#9681';
+    }
+    else{
+        document.getElementById("search-state").innerHTML='&#9679';
+    }
+}
+changeSearchState();
+function searchAll(){
+    searchState="all";
+    changeSearchState();
+}
+function searchParent(){
+    searchState="parent";
+    changeSearchState();
+}
+function searchChild(){
+    searchState="child";
+    changeSearchState();
+}
+
+
 
 'use strict';
 const superagent = require('superagent');
